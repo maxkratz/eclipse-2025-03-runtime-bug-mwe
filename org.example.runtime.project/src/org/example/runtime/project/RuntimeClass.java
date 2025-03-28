f4b97fd9fd81711984ed22e5c9b69435841b3f62
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-03-28 09:49:50 +0100

    Adds the project for the runtime workspace

---
+++ org.example.runtime.project/src/org/example/runtime/project/RuntimeClass.java
@@ -0,0 +1,12 @@
+package org.example.runtime.project;
+
+import org.example.dev.project.DevClass;
+
+public class RuntimeClass {
+
+	public static void main(final String[] args) {
+		final DevClass sut = new DevClass();
+		sut.sayHello();
+	}
+
+}
